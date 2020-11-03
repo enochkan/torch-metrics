a868040dadcddc4d08fc00ae2c608ad8edfcab8a
kanxx030 <kanxx030@gmail.com>
2020-11-03 13:09:11 -0600

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="torchmetrics",
+    version="1.0.0",
+    description="Metrics for model evaluation in pytorch",
+    url="https://github.com/sksq96/pytorch-summary",
+    author="Chi Nok Enoch Kan @chinokenochkan",
+    author_email="kanxx030@gmail.com",
+    packages=["torchmetrics"],
+)
---
+++ torch-metrics/torch-metrics.py
@@ -0,0 +1,18 @@
+import torch
+
+def corrcoef(tensor1, tensor2):
+    xm = x.sub(torch.mean(tensor1))
+    ym = y.sub(torch.mean(tensor2))
+    r_num = xm.dot(ym)
+    r_den = torch.norm(xm, 2) * torch.norm(ym, 2)
+    r_val = r_num / r_den
+    return r_val
+
+def RSquaredMetric(tensor1, tensor2):
+    return (corrcoef(tensor1, tensor2))**2
+
+def MSEMetric(tensor1, tensor2):
+    return torch.mean((tensor1 - tensor2)**2)
+
+def MAEMetric(tensor1, tensor2):
+    return torch.mean((tensor1 - tensor2))
