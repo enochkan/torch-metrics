4351483997720818d321d5c9710f4b6c5bf4a318
Haswanth Kumar Aekula <haswanth.kumar.39@gmail.com>
2020-11-04 11:35:10 +0530

    Update mae.py
    Taking absolute value

--- torch_metrics/mae.py
+++ torch_metrics/mae.py
@@ -8,4 +8,4 @@ class MAEMetric:
         x : torch.Tensor
         y : torch.Tensor
         """
-        return torch.mean((tensor1 - tensor2))
+        return torch.mean(torch.abs(tensor1 - tensor2))
