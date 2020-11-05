fixed bug where precision recall is not found

torch_metrics/tests/func_testing.py
@@ -1,7 +1,7 @@
-from torch_metrics import MSEMetric, MAEMetric, RSquaredMetric
+from torch_metrics import MeanSquaredError
 import torch
 
-metric = MSEMetric()
+metric = MeanSquaredError()
 t1 = torch.tensor([1., 2., 3.])
 t2 = torch.tensor([1., 5., 25.])
 print(metric(t1, t2))